6e7587efd2944a6b00fd4269b58e5bebe4de75b2
= <=>
2024-07-18 02:22:55 +0800

    instance name fix

--- login_auto_deploy.py
+++ login_auto_deploy.py
@@ -631,7 +631,7 @@ if __name__ == "__main__":
     cmd = os.getenv("ENV_CMD")
     with open('default_config.json', 'r') as f:
         defaultConfig = json.load(f)
-    with open('user_info2.json', 'r') as f:
+    with open('user_info.json', 'r') as f:
         accounts = json.load(f)
     try:
         with open('env_config.json', 'r') as f:
